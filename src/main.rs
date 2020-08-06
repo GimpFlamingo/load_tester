2a8393c55c2f6254e092902f43a6d09aae7095e9
Ray Kochenderfer <ray.kochenderfer@nextgenfilms.com>
2020-08-06 11:41:24 -0400

    Add statistics.rs

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
 use ron::de::from_reader;
-use std::{collections::HashMap, env, error::Error, fs::File};
+use std::{
+    collections::HashMap,
+    env,
+    error::Error,
+    fs::File,
+    time::Instant,
+};
 
 mod load;
 mod models;
+mod statistics;
 
 use crate::load::run_load_test;
 use models::Config;
@@ -20,7 +27,10 @@ fn load_config() -> Result<Config, Box<dyn Error>> {
 async fn main() -> Result<(), Box<dyn Error>> {
     // Load config file
     let config = load_config()?;
-    run_load_test(&config).await?;
+    let start_time = Instant::now();
+    let mut load_results = run_load_test(&config).await?;
+    let end_time = Instant::now();
+    load_results.total_time = end_time.duration_since(start_time).as_secs_f64();
 
     Ok(())
 }
