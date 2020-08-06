3d25c1d9cbfd8dd332b5cc998da6b63c451646bd
Ray Kochenderfer <ray.kochenderfer@nextgenfilms.com>
2020-08-06 11:23:13 -0400

    Add run_load_test and configuration

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,26 @@
 use ron::de::from_reader;
-use std::{
-    fs::File,
-    env,
-    error::Error,
-};
+use std::{collections::HashMap, env, error::Error, fs::File};
 
+mod load;
 mod models;
+
+use crate::load::run_load_test;
+use models::Config;
+
+/// Loads the config.ron file into a struct
+fn load_config() -> Result<Config, Box<dyn Error>> {
+    let input_path = format!("{}/config.ron", env!("CARGO_MANIFEST_DIR"));
+    let f = File::open(&input_path)?;
+    let config: Config = from_reader(f)?;
+
+    Ok(config)
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let input_path = format!("{}/config.ron", env!("CARGO_MANIFEST_DIR"));
-    let f = File::open(&input_path)?;
-    let config: models::Config = from_reader(f)?;
-
-    println!("Config: {:?}", &config);
+    // Load config file
+    let config = load_config()?;
+    run_load_test(&config).await?;
 
     Ok(())
 }
