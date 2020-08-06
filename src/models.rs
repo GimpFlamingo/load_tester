f0aa5be2df606c3902121c48ae3a1f9ae9f780b0
Ray Kochenderfer <ray.kochenderfer@nextgenfilms.com>
2020-08-06 09:42:34 -0400

    Add RequestResult and LoadResult models

--- src/models.rs
+++ src/models.rs
@@ -1,7 +1,24 @@
 use serde::Deserialize;
+use std::time::Duration;
 
 #[derive(Debug, Deserialize)]
 pub struct Config {
     pub urls: Vec<String>,
     pub runs: i32,
+}
+
+#[derive(Debug)]
+pub struct RequestResult {
+    pub url: String,
+    pub status: u32,
+    pub duration: Duration,
+}
+
+pub struct LoadResults {
+    pub total_requests: usize,
+    pub successes: usize,
+    pub failures: usize,
+    pub total_time: f64,
+    pub mean_request_time: f64,
+    pub median_request_time: f64,
 }
